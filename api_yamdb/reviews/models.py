6764eebdd2bce9838906103cff56f49eabb67332
Михаил Смирнов <rucka.1@yandex.ru>
2024-11-03 18:21:41 +0500

    removed unused imports

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator
 from django.db import models
 
 from .constants import (CHARFIELD_MAX_LENGTH, NAME_MAX_LENGTH)
